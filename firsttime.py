807f711b478c8d25c858adb5528d79ecff6542a7
Jon Briggs <jondanbriggs003@gmail.com>
2021-08-19 00:32:02 -0400

    Create firsttime.py

---
+++ firsttime.py
@@ -0,0 +1,8 @@
+print("Hello World!!!!")
+
+print("Please input your name.")
+
+firstname = input("Enter your first name here : ")
+lastname = input("Enter your last name here : ")
+print("Your name is",firstname,lastname)
+
